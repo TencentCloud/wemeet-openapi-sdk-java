ff80139a678370a0936d983dd0a079a0dec8ec1b
allenzhhou <allenzhhou@tencent.com>
2025-05-08 21:57:46 +0800

    1.更新sdk版本至v1.0.9

--- wemeet-openapi/src/main/java/com/tencentcloudapi/wemeet/core/Version.java
+++ wemeet-openapi/src/main/java/com/tencentcloudapi/wemeet/core/Version.java
@@ -4,5 +4,5 @@ public final class Version {
 
     private Version() {}
 
-    public static final String VERSION = "openapi-sdk-java/v1.0.6";
+    public static final String VERSION = "openapi-sdk-java/v1.0.9";
 }
